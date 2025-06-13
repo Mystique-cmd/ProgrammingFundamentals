7df7c86cd2c4fecb3132b76f75df4c1e2c7e68ef
Mystique Soul <mystique@Black-Hat.Black-Hat>
2025-06-13 16:19:01 +0300

    Added the create and remove breakpoint functions

--- custom_debugger.py
+++ custom_debugger.py
@@ -5,6 +5,19 @@ import struct
 import os
 import sys
 
+#Making the debugger cross-platform
+class Debugger:
+    def __init__(self, pid):
+        self.pid = pid
+    
+    def attach(self):
+        if sys.platform.startswith('win'):
+            kernel32 = ctypes.windll.kernel32
+            kernel32.DebugActiveProcess(self.pid)
+        else:
+            os.system(f"gdb -p {self.pid}")
+        pass
+
 #list all running processes
 for proc in psutil.process_iter(['pid', 'name']):# iterate over all processes on the system and returns the PID and name only 
     try:
@@ -51,3 +64,19 @@ if __name__ == "__main__":
     print(f"Registers for PID {pid}:")
 for field in regs._fields_:
     print(f"{field[0]}: {getattr(regs, field[0])}")
+
+#Step 3: Setting Breakpoints
+def set_Breakpoints(self, address):
+    if sys.platform.startswith('win'):
+        kernel32 = ctypes.windll.kernel32
+        kernel32.WriteProcessMemory(self.pid, address, b'\xCC', 1, None)
+    else:
+        libc.ptrace(7, self.pid, address, 0)  # PTRACE_POKETEXT
+    print(f"Breakpoint set at {hex(address)}")
+def remove_Breakpoints(self, address):
+    if sys.platform.startswith('win'):
+        kernel32 = ctypes.windll.kernel32
+        kernel32.WriteProcessMemory(self.pid, address, b'\x00', 1, None)
+    else:
+        libc.ptrace(7, self.pid, address, 0)  # PTRACE_POKETEXT
+    print(f"Breakpoint removed at {hex(address)}")  
